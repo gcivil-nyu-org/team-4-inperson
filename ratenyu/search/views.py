7358028bbffbea0198db010012737e0f8ca6acf5
ncolosso <colossonico@gmail.com>
2022-10-10 12:23:20 -0400

    Add style.css and ttf fonts

--- ratenyu/search/views.py
+++ ratenyu/search/views.py
@@ -1,6 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("This is our homepage/search page.")
-
+    return render(request, 'search/index.html', {})
