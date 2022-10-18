4ea66d63ea0c78d9b4c8dcef2a153e1a4fba927e
meoriordan <meoriordan@gmail.com>
2022-10-18 10:41:25 -0400

    create url, views, template for professor detail

--- ratenyu/professors/views.py
+++ ratenyu/professors/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
 
 # Create your views here.
+def professor_detail(request: HttpRequest, professor_id: str):
+    return render(request, "professors/detail.html")
