8afc88270a14723ad047ea42b0b7788c8e7737b0
Viren Parmar <virenmparmar@gmail.com>
2022-10-19 23:58:07 -0400

    Mapped search by courseid to course results

--- ratenyu/search/urls.py
+++ ratenyu/search/urls.py
@@ -5,6 +5,9 @@ from . import views
 app_name = 'search'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:professor_name>/professor_result', views.professor_result, name='professor_result'),
-    path('<str:course_name>/course_result', views.course_result, name='course_result'),
+    path('<str:professor_name>/professor_result',
+         views.professor_result, name='professor_result'),
+    path('<str:course_name>/course_result',
+         views.course_result, name='course_result'),
+    path('search_by_id', views.search_by_id_func, name='search_by_id'),
 ]
