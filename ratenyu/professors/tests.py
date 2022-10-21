9dc967f156a9c4d767d1310cc31f0254e8bfc30b
Nicolas Colosso <nvc9907@nyu.edu>
2022-10-21 17:52:28 -0400

    Travis Configuration (#114)
    * Initial Travis Setup
    
    * Initial Travis Setup
    
    * Travis Setup
    
    * more travis config
    
    * allowing build from all branches
    
    * Added comments to test travis
    
    * Testing Travis permissions
    
    * Changed ENV name for secret key
    
    * Dev & Prod settings updates
    
    * Add prod deployment
    
    * Add ENVs for dev and prod deployment
    
    * testing
    
    * testing
    
    * Install coveralls
    
    Co-authored-by: Viren Parmar <virenmparmar@gmail.com>

--- ratenyu/professors/tests.py
+++ ratenyu/professors/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Professor
+
+
+class ProfessorCreateTest(TestCase):
+    def setUp(self) -> None:
+        Professor.objects.create(
+            professor_id="1",
+            name="John Doe",
+            net_id="jd123",
+            role="Test Role"
+        )
+
+    def testProfessorCreate(self) -> None:
+        professor = Professor.objects.get(professor_id="1")
+        self.assertEqual(professor.name, "John Doe", "Failed to create test professor.")
