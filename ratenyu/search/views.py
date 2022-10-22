bb68eedf92f45da104e2690f092ba6414546d163
meoriordan <meoriordan@gmail.com>
2022-10-22 10:44:23 -0400

    fix formatting

--- ratenyu/search/views.py
+++ ratenyu/search/views.py
@@ -8,36 +8,43 @@ def index(request):
     return render(request, "search/index.html")
 
 
-def course_result(request, course_name):
-    return render(request, "search/courseResult.html")
-
-
-def professor_result(request, professor_name):
-    return render(request, "search/professorResult.html")
+# def course_result(request, course_name):
+#     return render(request, "search/courseResult.html")
+#
+#
+# def professor_result(request, professor_name):
+#     return render(request, "search/professorResult.html")
 
 
 def search_by_select(request: HttpRequest):
-    search_by = request.GET['search_by']
-    if search_by == 'CourseID':
+    search_by = request.GET["search_by"]
+    if search_by == "CourseID":
         return search_by_course_id(request)
-    elif search_by == 'CourseName':
+    elif search_by == "CourseName":
         return search_by_course_name(request)
-    elif search_by == 'ProfessorName':
+    elif search_by == "ProfessorName":
         return search_by_professor_name(request)
 
 def search_by_course_id(request: HttpRequest):
-    return render(request, 'search/courseResult.html')
+    return render(request, "search/courseResult.html")
 
 def search_by_course_name(request: HttpRequest):
-    query = request.GET['query']
-    courses = Course.objects.filter(course_title__contains=query)
-    context = {'courses':  courses}
-    return render(request, 'search/courseResult.html', context)
+    try:
+        query = request.GET["query"]
+        courses = Course.objects.filter(course_title__contains=query)
+        context = {"courses":  courses}
+        return render(request, "search/courseResult.html", context)
+    except:
+        raise Http404("Something went wrong")
 
 def search_by_professor_name(request):
-    query = request.GET['query']
-    professors = Professor.objects.filter(name__contains=query)
-    context = {'professors':  professors}
-    return render(request, 'search/professorResult.html', context)
+    try:
+        query = request.GET["query"]
+        professors = Professor.objects.filter(name__contains=query)
+        context = {"professors":  professors}
+        return render(request, "search/professorResult.html", context)
+    except:
+        raise Http404("Something went wrong")
+
 
 
