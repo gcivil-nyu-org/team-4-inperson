0054b9fd03098eb25b3019caf241ad69107e562e
meoriordan <meoriordan@gmail.com>
2022-10-22 10:32:22 -0400

    course results + professor results views using 'contains'

--- ratenyu/search/urls.py
+++ ratenyu/search/urls.py
@@ -6,9 +6,7 @@ app_name = "search"
 urlpatterns = [
     path("", views.index, name="index"),
     path(
-        "<str:professor_name>/professor_result",
-        views.professor_result,
-        name="professor_result",
-    ),
+        "<str:professor_name>/professor_result",views.professor_result,name="professor_result"),
     path("<str:course_name>/course_result", views.course_result, name="course_result"),
+    path("search_by_select", views.search_by_select, name="search_by_select")
 ]
--- ratenyu/search/views.py
+++ ratenyu/search/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest, Http404
+from courses.models import Course
+from professors.models import Professor
 
 
 def index(request):
@@ -11,3 +14,30 @@ def course_result(request, course_name):
 
 def professor_result(request, professor_name):
     return render(request, "search/professorResult.html")
+
+
+def search_by_select(request: HttpRequest):
+    search_by = request.GET['search_by']
+    if search_by == 'CourseID':
+        return search_by_course_id(request)
+    elif search_by == 'CourseName':
+        return search_by_course_name(request)
+    elif search_by == 'ProfessorName':
+        return search_by_professor_name(request)
+
+def search_by_course_id(request: HttpRequest):
+    return render(request, 'search/courseResult.html')
+
+def search_by_course_name(request: HttpRequest):
+    query = request.GET['query']
+    courses = Course.objects.filter(course_title__contains=query)
+    context = {'courses':  courses}
+    return render(request, 'search/courseResult.html', context)
+
+def search_by_professor_name(request):
+    query = request.GET['query']
+    professors = Professor.objects.filter(name__contains=query)
+    context = {'professors':  professors}
+    return render(request, 'search/professorResult.html', context)
+
+
