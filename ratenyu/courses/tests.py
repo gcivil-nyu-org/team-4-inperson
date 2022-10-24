f52298f811dba495309af86b2544bda07065ac95
ncolosso <colossonico@gmail.com>
2022-10-24 16:11:37 -0400

    Added HTML request test with Django Request Factory

--- ratenyu/courses/tests.py
+++ ratenyu/courses/tests.py
@@ -1,27 +1,45 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.utils import timezone
 from .models import Course
+from .views import course_detail
 from .course_util import *
 from professors.models import Professor
 
 
-class CourseDetailHelpersTest(TestCase):
+class TestDetailPageRequest(TestCase):
+    def setUp(self) -> None:
+        self.factory = RequestFactory()
+        create_test_course()
+
+    def testCourseCreated(self) -> None:
+        course = Course.objects.get(pk="1")
+        self.assertIsInstance(course, Course, "Test course not created.")
+
+    def testValidRequest(self) -> None:
+        course_id = "1"
+        request_str = f"http://127.0.0.1:8000/courses/{course_id}"
+        request = self.factory.get(request_str)
+        response = course_detail(request=request, course_id=course_id)
+        self.assertEqual(200, response.status_code, f"Request returned {response.status_code} for request {request_str}")
+
+
+class TestDetailPageHelpers(TestCase):
     def setUp(self) -> None:
         create_test_course()
         create_test_professor()
         create_test_class_1(
-            course=Course.objects.get(pk="170776"),
+            course=Course.objects.get(pk="1"),
             professor=Professor.objects.get(pk="1")
         )
         create_test_class_2(
-            course=Course.objects.get(pk="170776"),
+            course=Course.objects.get(pk="1"),
             professor=Professor.objects.get(pk="1")
         )
         create_test_review_1(Class.objects.get(pk="1"))
         create_test_review_2(Class.objects.get(pk="1"))
 
     def test_helper_functions(self) -> None:
-        test_course = Course.objects.get(pk="170776")
+        test_course = Course.objects.get(pk="1")
         review_objects = create_review_objects(Class.objects.filter(course=test_course))
         self.assertEqual(2, len(review_objects), f"Expected 2 reviews, found {len(review_objects)}.")
         self.assertEqual(3.0, calculate_rating_avg(review_objects))
@@ -29,7 +47,7 @@ class CourseDetailHelpersTest(TestCase):
 
 def create_test_course() -> Course:
     return Course.objects.create(
-        course_id="170776",
+        course_id="1",
         course_title="test course",
         course_subject_code="TS",
         catalog_number="1000",
