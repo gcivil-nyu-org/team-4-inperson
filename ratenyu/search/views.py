217ba05c16a75399c34560a85f5c870abe47bdc2
ncolosso <colossonico@gmail.com>
2022-10-23 20:35:55 -0400

    Added some additional constricts to simple filter

--- ratenyu/search/views.py
+++ ratenyu/search/views.py
@@ -1,19 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpRequest, Http404
+from django.db.models import Q
 from courses.models import Course
 from professors.models import Professor
 
 
 def index(request):
     return render(request, "search/index.html")
-
-
-# def course_result(request, course_name):
-#     return render(request, "search/courseResult.html")
-#
-#
-# def professor_result(request, professor_name):
-#     return render(request, "search/professorResult.html")
 
 
 def search_by_select(request: HttpRequest):
@@ -25,27 +18,35 @@ def search_by_select(request: HttpRequest):
     elif search_by == "ProfessorName":
         return search_by_professor_name(request)
 
+
 def search_by_course_id(request: HttpRequest):
     return render(request, "search/courseResult.html")
+
 
 def search_by_course_name(request: HttpRequest):
     try:
         query = request.GET["query"]
-        courses = Course.objects.filter(course_title__contains=query)
+        courses = Course.objects.filter(
+            Q(course_title__startswith=f"{query} ") |
+            Q(course_title__contains=f" {query} ") |
+            Q(course_title__endswith=f" {query}")
+        )
         context = {"courses":  courses}
         return render(request, "search/courseResult.html", context)
     except:
         raise Http404("Something went wrong")
 
+
 def search_by_professor_name(request):
     try:
         query = request.GET["query"]
-        professors = Professor.objects.filter(name__contains=query)
+        professors = Professor.objects.filter(
+            Q(name__startswith=f"{query} ") |
+            Q(name__contains=f" {query} ") |
+            Q(name__endswith=f" {query}")
+        )
         context = {"professors":  professors}
         return render(request, "search/professorResult.html", context)
     except:
         raise Http404("Something went wrong")
-
-
-
 
