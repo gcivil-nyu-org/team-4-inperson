f61cf1e45d3bc744b1d09c5e139fe4e62fa331cd
Nicolas Colosso <nvc9907@nyu.edu>
2022-10-25 13:59:16 -0400

    Pulling Latest Dev Changes into Master (#118)
    * Link professor course (#108)
    
    * add the course detail page
    
    * add the link between professor and course page
    
    * Update README.md
    
    * Update README.md
    
    * Travis Configuration (#114)
    
    * Initial Travis Setup
    
    * Initial Travis Setup
    
    * Travis Setup
    
    * more travis config
    
    * allowing build from all branches
    
    * Added comments to test travis
    
    * Testing Travis permissions
    
    * Changed ENV name for secret key
    
    * Dev & Prod settings updates
    
    * Add prod deployment
    
    * Add ENVs for dev and prod deployment
    
    * testing
    
    * testing
    
    * Install coveralls
    
    Co-authored-by: Viren Parmar <virenmparmar@gmail.com>
    
    * Allow deploy from dev and prod
    
    * Update README.md
    
    * Travis config
    
    * Trying to modularize eb deployment
    
    * trying to modularize EB deployment
    
    * travis config
    
    * Black syntax changes
    
    * travis config
    
    * travis config
    
    * EB config
    
    * eb config
    
    * eb config
    
    * eb config
    
    * eb config
    
    * eb config
    
    * eb config
    
    * eb config
    
    * eb config
    
    * eb config
    
    * Add tags to Prod env in README
    
    * Modularized course detail view and added tests (#116)
    
    * Modularized course detail view and added tests
    
    * Removed HTTP request test for now
    
    * Added HTML request test with Django Request Factory
    
    Co-authored-by: Haofan Wang <90638309+N17270182@users.noreply.github.com>
    Co-authored-by: Viren Parmar <virenmparmar@gmail.com>

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Dev - http://ratenyu-dev.eba-apngxcqy.us-east-1.elasticbeanstalk.com/search/
 
 
 Prod - http://ratenyu.eba-apngxcqy.us-east-1.elasticbeanstalk.com/search/
+&nbsp;
+[![Build Status](https://app.travis-ci.com/gcivil-nyu-org/team-4-inperson.svg?branch=master)](https://app.travis-ci.com/gcivil-nyu-org/team-4-inperson)
+&nbsp;
+[![Coverage Status](https://coveralls.io/repos/github/gcivil-nyu-org/team-4-inperson/badge.svg?branch=master)](https://coveralls.io/github/gcivil-nyu-org/team-4-inperson?branch=master)
--- ratenyu/courses/tests.py
+++ ratenyu/courses/tests.py
@@ -1,3 +1,112 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.utils import timezone
+from .models import Course
+from .views import course_detail
+from .course_util import *
+from professors.models import Professor
 
-# Create your tests here.
+
+class TestDetailPageRequest(TestCase):
+    def setUp(self) -> None:
+        self.factory = RequestFactory()
+        create_test_course()
+
+    def testCourseCreated(self) -> None:
+        course = Course.objects.get(pk="1")
+        self.assertIsInstance(course, Course, "Test course not created.")
+
+    def testValidRequest(self) -> None:
+        course_id = "1"
+        request_str = f"http://127.0.0.1:8000/courses/{course_id}"
+        request = self.factory.get(request_str)
+        response = course_detail(request=request, course_id=course_id)
+        self.assertEqual(200, response.status_code, f"Request returned {response.status_code} for request {request_str}")
+
+
+class TestDetailPageHelpers(TestCase):
+    def setUp(self) -> None:
+        create_test_course()
+        create_test_professor()
+        create_test_class_1(
+            course=Course.objects.get(pk="1"),
+            professor=Professor.objects.get(pk="1")
+        )
+        create_test_class_2(
+            course=Course.objects.get(pk="1"),
+            professor=Professor.objects.get(pk="1")
+        )
+        create_test_review_1(Class.objects.get(pk="1"))
+        create_test_review_2(Class.objects.get(pk="1"))
+
+    def test_helper_functions(self) -> None:
+        test_course = Course.objects.get(pk="1")
+        review_objects = create_review_objects(Class.objects.filter(course=test_course))
+        self.assertEqual(2, len(review_objects), f"Expected 2 reviews, found {len(review_objects)}.")
+        self.assertEqual(3.0, calculate_rating_avg(review_objects))
+
+
+def create_test_course() -> Course:
+    return Course.objects.create(
+        course_id="1",
+        course_title="test course",
+        course_subject_code="TS",
+        catalog_number="1000",
+        course_description="course_description"
+    )
+
+
+def create_test_professor() -> Professor:
+    return Professor.objects.create(
+        professor_id="1",
+        name="John Doe",
+        net_id="jd123",
+        role="01"
+    )
+
+
+def create_test_class_1(course: Course, professor: Professor) -> Class:
+    return Class.objects.create(
+        class_id="1",
+        professor=professor,
+        course=course,
+        class_type="Test",
+        class_section="1",
+        term="Jan2022",
+        last_offered="Jan2022",
+        location="WS",
+        enroll_capacity=100
+    )
+
+
+def create_test_class_2(course: Course, professor: Professor) -> Class:
+    return Class.objects.create(
+        class_id="2",
+        professor=professor,
+        course=course,
+        class_type="Test",
+        class_section="2",
+        term="Fall2023",
+        last_offered="Fall2023",
+        location="BK",
+        enroll_capacity=50
+    )
+
+
+def create_test_review_1(class_id: Class) -> Review:
+    return Review.objects.create(
+        review_text="I love this professor!",
+        rating=5,
+        class_id=class_id,
+        user="User1",
+        pub_date=timezone.now()
+    )
+
+
+def create_test_review_2(class_id: Class) -> Review:
+    return Review.objects.create(
+        review_text="I hate this professor!",
+        rating=1,
+        class_id=class_id,
+        user="User2",
+        pub_date=timezone.now()
+    )
