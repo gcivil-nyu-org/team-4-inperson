31003c86cc67fd9d0232d165ea71c2872d664e07
meoriordan <meoriordan@gmail.com>
2022-10-25 14:50:50 -0400

    add tests for course results and fix formatting on search_util

--- ratenyu/search/search_util.py
+++ ratenyu/search/search_util.py
@@ -3,7 +3,7 @@ from courses.models import Course, Class, Review
 from professors.models import Professor
 from courses.course_util import *
 
-def course_query(query):
+def course_query(query: str) -> Class:
     courses = Course.objects.filter(
         Q(course_title__startswith=f"{query} ") |
         Q(course_title__contains=f" {query} ") |
@@ -11,7 +11,7 @@ def course_query(query):
     )
     return courses
 
-def professor_query(query):
+def professor_query(query: str) -> Class:
     professors = Professor.objects.filter(
         Q(name__startswith=f"{query} ") |
         Q(name__contains=f" {query} ") |
@@ -21,7 +21,7 @@ def professor_query(query):
 
 #this returns a dictionary that contains the
 # info necessary to display a course on course results page
-def get_course_results_info(course) -> dict:
+def get_course_results_info(course: Class) -> dict:
     classes = Class.objects.filter(course=course.course_id)
     reviews_list = create_review_objects(classes)
     reviews_avg = calculate_rating_avg(reviews_list)
