74f7d85e61a61544ff2975f2929e11c3a2f1bf6a
Viren Parmar <virenmparmar@gmail.com>
2022-10-25 16:00:04 -0400

    "Added url for error page"

---
+++ ratenyu/util/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+app_name = "util"
+urlpatterns = [
+    path("error", views.error404, name="error404"),
+]
--- ratenyu/util/views.py
+++ ratenyu/util/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 
-def error404(request, error=None):
+def error404(request, error=""):
     return render(request, "error.html", {"error": error})
