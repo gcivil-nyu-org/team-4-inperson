f0066e64390a6cfda86effd18c18b3d3e66de718
ncolosso <colossonico@gmail.com>
2022-10-25 15:47:02 -0400

    Added urls change from Elizabeth's branch and small reformat

--- ratenyu/ratenyu/urls.py
+++ ratenyu/ratenyu/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("search/", include("search.urls")),
+    path("", include("search.urls")),
     path("admin/", admin.site.urls),
     path("professors/", include("professors.urls")),
     path("courses/", include("courses.urls")),
