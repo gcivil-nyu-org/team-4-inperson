12df0c4884790a8c7f50a74ca9c0a1b7f931bf10
meoriordan <meoriordan@gmail.com>
2022-10-25 13:53:58 -0400

    modularize course results view

--- ratenyu/search/views.py
+++ ratenyu/search/views.py
@@ -3,6 +3,8 @@ from django.http import HttpRequest, Http404
 from django.db.models import Q
 from courses.models import Course, Class, Review
 from professors.models import Professor
+from .search_util import *
+from courses.course_util import *
 
 
 def index(request):
@@ -26,47 +28,14 @@ def search_by_course_id(request: HttpRequest):
 def search_by_course_name(request: HttpRequest):
     try:
         query = request.GET["query"]
-        courses = Course.objects.filter(
-            Q(course_title__startswith=f"{query} ") |
-            Q(course_title__contains=f" {query} ") |
-            Q(course_title__endswith=f" {query}") |
-            Q(course_title=query)
-        )
-
+        courses = course_query(query)
         filtered_courses = []
         for i in courses:
-            reviews_list = []
-            reviews_rating_list = []
-            classes = Class.objects.filter(course=i.course_id)
-            for cl in classes:
-                review_set = Review.objects.filter(class_id=cl.class_id)
-                for rev in review_set:
-                    current_review = {
-                        "review_obj": rev
-                    }
-                    reviews_list.append(current_review)
-                    reviews_rating_list.append(rev.rating)
-            if len(reviews_rating_list) > 0:
-                reviews_avg = round(
-                    float(sum(reviews_rating_list) / len(reviews_rating_list)), 1
-                )
-            else:
-                reviews_avg = 0
-
-            current_course = {
-                    "course_id": i.course_id,
-                    "course_title":i.course_title,
-                    "course_subject_code":i.course_subject_code,
-                    "catalog_number":i.catalog_number,
-                    "course_description":i.course_description,
-                    "reviews_list":reviews_list,
-                    "reviews_avg":reviews_avg
-                }
-
-            filtered_courses.append(current_course)
+            current_course_info = get_course_results_info(i)
+            filtered_courses.append(current_course_info)
         context = {
             "courses":  filtered_courses,
-            "query" : query,
+            "query": query,
         }
         return render(request, "search/courseResult.html", context)
     except:
