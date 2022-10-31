f5618fa4b5e92b1f8009639e824488053f739f5d
meoriordan <meoriordan@gmail.com>
2022-10-31 18:32:19 -0400

    add some tests

--- ratenyu/search/tests.py
+++ ratenyu/search/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase, RequestFactory
 from .search_util import *
-from courses.models import Course
+from courses.models import Course, Class
 from professors.models import Professor
 from .views import search_by_course_name
+from courses.tests import *
 
 
 # Create your tests here.
@@ -71,3 +72,28 @@ class TestSearchFiltering(TestCase):
     def test_non_matching_professor_name_query(self) -> None:
         query = "John"
         self.assertQuerysetEqual(professor_query(query), [])
+
+class TestSearchPageHelpers(TestCase):
+    def setUp(self) -> None:
+        create_test_course()
+        create_test_professor()
+        create_test_class_1(
+            course=Course.objects.get(pk="1"), professor=Professor.objects.get(pk="1")
+        )
+        create_test_class_2(
+            course=Course.objects.get(pk="1"), professor=Professor.objects.get(pk="1")
+        )
+        create_test_review_1(Class.objects.get(pk="1"))
+        create_test_review_2(Class.objects.get(pk="1"))
+
+    def test_course_helper_function(self) -> None:
+        test_course = Course.objects.get(pk="1")
+        d = get_course_results_info(test_course)
+        self.assertEqual(2, len(d['reviews_list']))
+        self.assertEqual(3.0, d['reviews_avg'])
+
+    def test_professor_helper_function(self) -> None:
+        test_professor = Professor.objects.get(pk="1")
+        d = get_professor_results_info(test_professor)
+        self.assertEqual(2, len(d['reviews_list']))
+        self.assertEqual(3.0, d['reviews_avg'])
