2342c122bab2208e3202ae8db991f6a25c3316d4
ncolosso <colossonico@gmail.com>
2022-11-01 18:29:25 -0400

    Integrated Login page w Open ID and ran Black

--- ratenyu/users/urls.py
+++ ratenyu/users/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-
 from . import views
 
 app_name = "users"
@@ -13,7 +12,7 @@ urlpatterns = [
     ),
     path(
         "logout/",
-        auth_views.LogoutView.as_view(template_name="users/logout.html"),
+        auth_views.LogoutView.as_view(template_name="search/index.html"),
         name="logout",
     ),
 ]
