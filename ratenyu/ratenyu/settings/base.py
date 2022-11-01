2854fa5a9c4ec4dcfb250daa7692240343637abc
Haofan Wang <90638309+N17270182@users.noreply.github.com>
2022-11-01 13:16:20 -0400

    Registration page and login page (#133)
    * Registration/Login functionality
    
    * Added login/logout functionality. User details are stored in DB
    
    * modify the search button to match with the registration button
    
    * add navbar logged in state
    
    * fix formatting issue on homepage
    
    * Reformatted Login UI & ran Black
    
    * Rebased and reformatted Logout page
    
    Co-authored-by: Viren Parmar <virenmparmar@gmail.com>
    Co-authored-by: meoriordan <meoriordan@gmail.com>
    Co-authored-by: ncolosso <colossonico@gmail.com>

--- ratenyu/ratenyu/settings/base.py
+++ ratenyu/ratenyu/settings/base.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "courses",
     "professors",
     "util",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,5 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "search:index"
