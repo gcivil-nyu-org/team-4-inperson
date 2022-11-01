08b5c43de4c1f8ba4fedd6ed627a1998a8b9bb04
Viren Parmar <virenmparmar@gmail.com>
2022-11-01 15:18:40 -0400

    Changed conditions to create log folder inside projects

--- ratenyu/util/management/commands/create_log_folder.sh
+++ ratenyu/util/management/commands/create_log_folder.sh
@@ -1,5 +1,5 @@
 echo "creating logs folder"
-chmod 777 ../../../../../../
-mkdir mkdir -p ../../../../../logs
-echo > ../../../../../logs/django.log
+chmod 777 ../../../../../
+mkdir -p ../../../../logs
+echo > ../../../../logs/django.log
 echo "logs folder created"
