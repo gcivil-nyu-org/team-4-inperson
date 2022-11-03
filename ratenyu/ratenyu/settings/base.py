e6e3141f4d4c65684bbae63f27d223ccadb436cb
Viren Parmar <virenmparmar@gmail.com>
2022-11-02 21:19:06 -0400

    Addded changes to print logs on the console

--- ratenyu/ratenyu/settings/base.py
+++ ratenyu/ratenyu/settings/base.py
@@ -130,7 +130,7 @@ LOGGING = {
     'disable_existing_loggers': False,
     'formatters': {
         'verbose': {
-            'format': '%(asctime)s %(levelname)s %(filename)s %(message)s'
+            'format': '[%(asctime)s] [%(levelname)s] %(filename)s %(message)s'
         }
     },
     'handlers': {
@@ -140,10 +140,16 @@ LOGGING = {
             'filename': str(PATH_TO_LOGS_FOLDER) + '/django.log',
             'formatter': 'verbose',
         },
+        'console1': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+            'stream': 'ext://sys.stdout'
+        }
     },
     'loggers': {
         'project': {
-            'handlers': ['debug1'],
+            'handlers': ['debug1', 'console1'],
             'level': 'DEBUG',
             'propagate': True,
         },
