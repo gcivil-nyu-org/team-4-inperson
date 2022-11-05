1d4a7a117580b084482735fa003c9d400d24d2b0
Viren Parmar <virenmparmar@gmail.com>
2022-11-05 12:57:39 -0400

    Initial filtering changes on results pages

---
+++ ratenyu/static/js/filtering.js
@@ -0,0 +1,28 @@
+function getListOfChecked() {
+  var listOfChecked = [];
+  var checkBoxes = document.querySelectorAll("input[type=checkbox]:checked");
+  for (var i = 0; i < checkBoxes.length; i++) {
+    listOfChecked.push(checkBoxes[i].id);
+  }
+  return listOfChecked;
+}
+function resultCheckBoxClicked() {
+  var listOfChecked = getListOfChecked();
+  console.log(listOfChecked);
+  var listOfItems = document.getElementsByClassName("detail-sub-course-desc")
+    for (var i = 0; i < listOfItems.length; i++) {
+        var element = listOfItems[i];
+        var isFound = false;
+        for (var j = 0; j < listOfChecked.length; j++) {
+            if (element.textContent.includes(listOfChecked[j])) {
+                isFound = true;
+                break;
+            }
+        }
+        if (isFound) {
+            element.style.display = "block";
+        } else {
+            element.style.display = "none";
+        }
+    }
+}
