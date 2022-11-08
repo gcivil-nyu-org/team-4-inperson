a1c94528ef06d25d9b05c6bc59120bf1818ba86e
Haofan Wang <90638309+N17270182@users.noreply.github.com>
2022-11-07 22:31:36 -0500

    revert nav2 and changed setting

--- ratenyu/ratenyu/settings/base.py
+++ ratenyu/ratenyu/settings/base.py
@@ -151,5 +151,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'haofanw364903129@gmail.com'
-EMAIL_HOST_PASSWORD = 'sednnrlkwqxetodk'
+EMAIL_HOST_USER = 'ratenyuteam@gmail.com'
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
