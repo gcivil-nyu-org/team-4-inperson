f40c9b5c016b1c545508f5511757b067498cb4a7
meoriordan <meoriordan@gmail.com>
2022-11-08 21:57:09 -0500

    fix bug in course detail view

--- ratenyu/courses/views.py
+++ ratenyu/courses/views.py
@@ -18,7 +18,7 @@ def course_detail(request: HttpRequest, course_id: str):
         professors_list = [cl.professor for cl in classes]
         reviews_list = create_review_objects(classes)
         reviews_avg = calculate_rating_avg(reviews_list)
-        context = {"classes": classes, "course": course, "reviews_list": reviews_list}
+        context = {"classes": classes, "course": course, "reviews_list": reviews_list,"reviews_avg": reviews_avg, "professors_list": professors_list}
         return render(request, "courses/detail.html", context)
     except Exception:
         raise Http404("Course does not exist")
