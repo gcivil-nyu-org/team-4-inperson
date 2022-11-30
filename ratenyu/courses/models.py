f691e19cdf8bd530595a9f8756a141484d74a20d
Viren Parmar <virenmparmar@gmail.com>
2022-11-30 16:35:15 -0500

    Votes (#264)
    * Like functionality initial logic
    
    * Added reverting of like.
    
    * Replace Like/Dislike buttons with Thumbs Up/Thumbs Down buttons
    
    * Added backend for dislike
    
    * Like/Dislike button coloring
    
    * Moved like/dislike functionality to Utils app. Added pre-loading of active thumbs up/thumbs down
    
    * Only show like/dislike if User is logged in
    
    * Added tests for voting
    
    * Add vote counter
    
    * Unauthorized user redirected to login page on like/dislike click
    
    * Removed duplicate variable name in views.py
    
    * Remove migration
    
    * Fixed merge
    
    * Added saved course
    
    Co-authored-by: ncolosso <colossonico@gmail.com>
    Co-authored-by: Ujjwal Kulkarni <uk2011@nyu.edu>

--- ratenyu/courses/models.py
+++ ratenyu/courses/models.py
@@ -45,3 +45,11 @@ class Vote(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     review = models.ForeignKey(to=Review, on_delete=models.CASCADE)
     vote = models.CharField(max_length=1, choices=(("L", "L"), ("D", "D")))
+
+class SavedCourse(models.Model):
+    user_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    course_id = models.ForeignKey(to=Course, on_delete=models.CASCADE)
+    professor_id = models.ForeignKey(to=Professor, on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        unique_together = ('user_id', 'course_id')
