f0bcc5dba58528856d46eb4fa03960d7749a3287
Viren Parmar <virenmparmar@gmail.com>
2022-11-30 16:26:58 -0500

    Added saved course

--- ratenyu/courses/admin.py
+++ ratenyu/courses/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Course, Class, Review, Vote
+from .models import Course, Class, Review, Vote, SavedCourse
 
 
 admin.site.register(Course)
 admin.site.register(Class)
 admin.site.register(Review)
 admin.site.register(Vote)
+admin.site.register(SavedCourse)
