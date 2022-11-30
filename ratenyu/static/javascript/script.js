6fad80f10ff58d441fde422b12d7185ca129fe2b
Haofan Wang <90638309+N17270182@users.noreply.github.com>
2022-11-29 21:24:07 -0500

    js problem sovled

--- ratenyu/static/javascript/script.js
+++ ratenyu/static/javascript/script.js
@@ -472,16 +472,16 @@ if ( window.history.replaceState ) {
    }
 
 
-// let placeholderText = {
-//     "CourseID": "example: CS-GY 6003",
-//     "CourseName": "example: Algorithms",
-//     "ProfessorName": "example: John Doe"
-// };
-//
-// function searchPlaceholder(){
-//
-//     let selection = document.getElementById("search_by");
-//     let inputBox = document.getElementById("query_search");
-//     let selectedVal = String(selection.value);
-//     inputBox.placeholder = String(placeholderText[selectedVal]);
-// }
+function searchPlaceholder(){
+
+    let placeholderText = {
+        "CourseID": "example: CS-GY 6003",
+        "CourseName": "example: Algorithms",
+        "ProfessorName": "example: John Doe"
+    };
+
+    let selection = document.getElementById("search_by");
+    let inputBox = document.getElementById("query_search");
+    let selectedVal = String(selection.value);
+    inputBox.placeholder = String(placeholderText[selectedVal]);
+}
