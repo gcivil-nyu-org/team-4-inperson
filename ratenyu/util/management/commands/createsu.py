1707521aa92ec50611f5668ac9391b2f179b07b2
Viren Parmar <virenmparmar@gmail.com>
2022-12-08 11:17:12 -0500

    removed hardcoded password

--- ratenyu/util/management/commands/createsu.py
+++ ratenyu/util/management/commands/createsu.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-
+from decouple import config
 
 class Command(BaseCommand):
     help = "Creates a superuser."
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(username="admin", password="rateNYU2022")
+            User.objects.create_superuser(username="admin", password=config("ADMIN_PWD"))
         print("Superuser has been created.")
